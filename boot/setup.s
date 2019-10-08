61ea205ef128fc970aa4202904a5510cea426506
xjk <2459169377@qq.com>
2019-10-08 17:27:58 +0800

    learn how to use Bochs to debug kernel
    1. estimate the linear address of breakpoint
    2. set breakpoint on 0x7e00
    3. watch the value that read from 0x90000(the address of storing
    		parameters)
    4. you will find dx is 0

--- boot/setup.s
+++ boot/setup.s
@@ -41,8 +41,8 @@ ENDSEG   = SYSSEG + SYSSIZE		! where to stop loading
 ! ROOT_DEV:	0x000 - same type of floppy as boot.
 !		0x301 - first partition on first drive etc
 
-entry _start
-_start:
+entry start
+start:
 ! Print some inane message
 
 	mov	ah,#0x03		! read cursor pos
@@ -59,12 +59,97 @@ _start:
 	mov es,ax
 	mov	ax,#0x1301		! write string, move cursor
 	int	0x10
-! ok, we've written the message, now
 
+
+	mov ax,cs
+	mov es,ax
+! init ss:sp
+	mov ax,#INITSEG
+	mov ss,ax
+	mov sp,0xff00
+! get params
+	mov ax,#INITSEG
+	mov ds,ax
+	mov ah,#0x03
+	xor bh,bh
+	int 0x10
+	mov [0],dx
+	mov ah,#0x88
+	int 0x15
+	mov [2],ax
+	mov ax,#0x0000
+	mov ds,ax
+	lds si,[4*0x41]
+	mov ax,#INITSEG
+	mov es,ax
+	mov di,#0x0004
+	mov cx,#0x10
+	rep
+	movsb
+! be ready to print
+	mov ax,cs
+	mov es,ax
+	mov ax,#INITSEG
+	mov ds,ax
+! Cursor Position
+	mov ah,#0x03
+	xor bh,bh
+	int 0x10
+	mov cx,#18
+	mov bx,#0x0007
+	mov bp,#msg_cursor
+	mov ax,#0x1301
+	int 0x10
+	mov dx,[0]
+	call print_hex
+! Memory Size
+	mov ah,#0x03
+	xor bh,bh
+	int 0x10
+	mov cx,#14
+	mov bx,#0x0007
+	mov bp,#msg_memory
+	mov ax,#0x1301
+	int 0x10
+	mov dx,[2]
+	call print_hex
 
 inf_loop:
 	jmp inf_loop
 
+
+print_hex:
+	mov cx,#4
+!???????????????????
+	mov dx,(bp)
+print_digit:
+	rol dx,#4
+	mov ax,#0xe0f
+	and al,dl
+	add al,#0x30
+	cmp al,#0x3a
+	jl outp
+	add al,#0x07
+outp:
+	int 0x10
+	loop print_digit
+	ret
+print_nl:
+! CR
+	mov ax,#0xe0d
+	int 0x10
+! LF
+	mov al,#0xa
+	int 0x10
+	ret
+
+msg_cursor:
+	.byte 13,10
+	.ascii "Cursor position:"
+
+msg_memory:
+	.byte 13,10
+	.ascii "Memory Size:"
 msg2:
 	.byte 13,10
 	.ascii "Now we are in SETUP"
