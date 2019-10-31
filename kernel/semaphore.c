fb283275893849acaaae98324df29c80ef3b365d
xjk <2459169377@qq.com>
2019-10-31 16:56:32 +0800

    have a very big problem
    1. How to call by pointer in system call ?(user space and kernel space)
    2. How to return pointer in system call ?

--- kernel/semaphore.c
+++ kernel/semaphore.c
@@ -3,45 +3,46 @@
 #include <asm/segment.h>
 #include <asm/system.h>
 typedef struct {
-		char *name;
+		char name[20];
 		int value;
 		struct task_struct *queue;
 } sem_t; 
 sem_t semtable[20] = {
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL},
-		{"\0", 0, NULL}
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL},
+		{{'\0',}, 0, NULL}
 };
 
 /*
  * if success return unique identifier > 0, else return NULL
  * */
 //sem_t *sys_sem_open(const char *name, unsigned int value)
+/*
 int sys_sem_open(const char *name, unsigned int value, sem_t **sem)
 {
 		//find sem named name in semtable
 		char myname[20];
 		const char *cur = name;
 		char c;
-		int i;
+		int i = 0;
 		while ((c = get_fs_byte(cur)) != '\0' && i < 19) {
 				myname[i] = c;
 				i++;
@@ -60,14 +61,45 @@ int sys_sem_open(const char *name, unsigned int value, sem_t **sem)
 						strcpy(semtable[i].name, myname);
 						semtable[i].value = value;
 						*sem = &semtable[i];
+						printk("create sucessful\n");
 						return &semtable[i];
 				}
-		*sem = &semtable[i];
 		return NULL;
 
 		//return index
 }
+*/
+int sys_sem_open(const char *name, unsigned int value, sem_t **sem)
+{
+		//find sem named name in semtable
+		char myname[20];
+		int i = 0;
 
+		for (i = 0; myname[i] = get_fs_byte(name + i); i++);
+		for (i = 0; i < 20; i++) {
+				if (!strcmp(semtable[i].name, myname)) {
+						*sem = &semtable[i];
+						return i;
+				}
+		}
+
+		for (i = 0; i < 20; i++) {
+				if (semtable[i].name[0] == '\0') {
+						strcpy(semtable[i].name, myname);
+						semtable[i].value = value;
+						semtable[i].queue = NULL;
+						*sem = &semtable[i];
+						printk("%s %p\n", myname,*sem);
+						return i;
+				}
+		}
+
+		printk("kernel sem open fail\n");
+		*sem = NULL;
+		return -1;
+
+}
+/*
 int sys_sem_wait(sem_t *sem)
 {
 		cli();
@@ -75,6 +107,18 @@ int sys_sem_wait(sem_t *sem)
 		while (sem->value-- < 0)
 				sleep_on(&sem->queue);
 
+		sti();
+		return 0;
+}
+*/
+int sys_sem_wait(sem_t *sem)
+{
+		cli();
+
+		while (sem->value <= 0) {
+				sleep_on(&(sem->queue));
+		}
+		sem->value--;
 		sti();
 		return 0;
 }
@@ -95,12 +139,23 @@ int sys_sem_wait(sem_t *sem)
 		sti();
 }
 */
-
+/*
 int sys_sem_post(sem_t *sem)
 {
 		cli();
-		if (sem->value++ <= 0)
+		//if (sem->value++ <= 0)
+		sem->value++;
 				wake_up(&sem->queue);
+		sti();
+		return 0;
+}
+*/
+int sys_sem_post(sem_t *sem)
+{
+		cli();
+		sem->value++;
+		wake_up(&(sem->queue));
+
 		sti();
 		return 0;
 }
@@ -121,7 +176,7 @@ int sys_sem_unlink(const char *name)
 
 		for (i = 0; i < 20; i++)
 				if (!strcmp(semtable[i].name, myname)) {
-						semtable[i].name = "\0";
+						semtable[i].name[0] = '\0';
 						semtable[i].value = 0;
 						semtable[i].queue = NULL;
 						return 0;
--- semaphore.c
+++ semaphore.c
@@ -4,7 +4,6 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-
 #define __LIBRARY__
 #include <unistd.h>
 
@@ -12,6 +11,18 @@
 #define __NR_sem_wait	73
 #define __NR_sem_post	74
 #define __NR_sem_unlink	75
+#define MAX 10
+
+
+int fd;
+int fill = 0;
+int use = 0;
+const int items = 500;
+const int cNums = 2;
+sem_t *empty;
+sem_t *full;
+sem_t *mutex;
+
 typedef struct {
 		char *name;
 		int value;
@@ -26,57 +37,54 @@ _syscall1(int, sem_post, sem_t*, sem)
 _syscall1(int, sem_unlink, const char*, name)
 	
 
-/*
-
-int main()
-{
-		if (sem_open("sem1", 1, &sem) == NULL)
-				printf("open error\n");
-		printf("wait\n");
-		if (sem_wait(sem) < 0)
-				printf("wait error \n");
-
-		return 0;
-}
-*
-*/
-
-#define MAX 10
-
-
-int fd;
-int fill = 0;
-int use = 0;
-
 void put(int value)
 {
 		if (lseek(fd, fill*sizeof(int), SEEK_SET) == -1)
 				printf("lseek error\n");
 		if (write(fd, &value, sizeof(int)) < 0)
 				printf("write error\n");
+		/*
+		printf("put %d in %d\n", value, fill);
+		fflush(stdout);
+		*/
 		fill = (fill + 1) % MAX;
 }
 
 int get()
 {
 		int tmp;
+		if (lseek(fd, MAX*sizeof(int), SEEK_SET) == -1)
+				printf("lseek error\n");
+		if (read(fd, &use, sizeof(int)) < 0)
+				printf("read error\n");
+
+
 		if (lseek(fd, use*sizeof(int), SEEK_SET) == -1)
 				printf("lseek error\n");
 		if (read(fd, &tmp, sizeof(int)) < 0)
 				printf("read error\n");
+		/*
+		printf("get %d in %d\n", tmp, use);
+		fflush(stdout);
+		*/
 		use = (use + 1) % MAX;
+
+
+		if (lseek(fd, MAX*sizeof(int), SEEK_SET) == -1)
+				printf("lseek error\n");
+		if (write(fd, &use, sizeof(int)) < 0)
+				printf("read error\n");
+
+
 		return tmp;
 }
 
 
-sem_t *empty;
-sem_t *full;
-sem_t *mutex;
 
 void *producer(void *arg) 
 {
 		int i;
-		int loops = 501;
+		int loops = items;
 		for (i = 0; i < loops; i++) {
 				sem_wait(empty);
 				sem_wait(mutex);
@@ -89,26 +97,17 @@ void *producer(void *arg)
 void *consumer(void *arg)
 {
 		int i;
-		int loops = 501;
 		int tmp;
 		pid_t pid;
 		pid = getpid();
-		/*
-		for (i = 0; i < loops; i++) {
-				sem_wait(&full);
-				sem_wait(&mutex);
-				int tmp = get();
-				sem_post(&mutex);
-				sem_post(&empty);
-				printf("%lu %d\n", (unsigned long)pid,tmp);
-		}
-		*/
-		while (1)
+		
+		for (int k = 0; k < items/cNums; k++)
 		{
 				sem_wait(full);
 				sem_wait(mutex);
 				tmp = get();
-				printf("%lu: %d\n", (unsigned long)pid,tmp);
+				printf("%lu: %d\n", (unsigned long)pid, tmp);
+				fflush(stdout);
 				sem_post(mutex);
 				sem_post(empty);
 		}
@@ -116,30 +115,31 @@ void *consumer(void *arg)
 
 int main(int argc, char *argv[])
 {
-		
-		pid_t c1, c2;
-		pid_t p1, p2;
 		if ((fd = open("buffer.txt", O_RDWR | O_CREAT | O_TRUNC)) < 0)
 				printf("open error\n");
+
+		if (lseek(fd, MAX*sizeof(int), SEEK_SET) == -1)
+				printf("lseek error\n");
+		if (write(fd, &use, sizeof(int)) < 0)
+				printf("read error\n");
 		/*
 		sem_init(&empty, 1, MAX);
 		sem_init(&full, 1, 0);
 		sem_init(&mutex, 1, 1);
 		*/
-		/*
-		empty = sem_open("/sem_empty", O_CREAT | O_EXCL, O_RDWR, MAX);
-		full = sem_open("/sem_full", O_CREAT | O_EXCL, O_RDWR, 0);
-		mutex = sem_open("/sem_mutex", O_CREAT | O_EXCL, O_RDWR, 1);
-		*/
-		if (sem_open("sem_empty", MAX, &empty) == 0)
-				printf("sem_empty error\n");
-		if (sem_open("sem_full", 0, &full) == 0)
-				printf("sem_full error\n");
-		if (sem_open("sem_mutex", 1, &mutex) == 0)
-				printf("sem_mutex error\n");
+		if (sem_open("sem_empty", MAX, &empty) < 0) {
+				printf("empty error\n");
+		}
+		if (sem_open("sem_full", 0, &full) < 0) {
+				printf("full error\n");
+		}
+		if (sem_open("sem_mutex", 1, &mutex) < 0) {
+				printf("mutex error\n");
+		}
 
 
-
+		pid_t c1, c2;
+		pid_t p1, p2;
 		if (!(p1 = fork()))
 		{
 				producer(NULL);
@@ -151,10 +151,15 @@ int main(int argc, char *argv[])
 				consumer(NULL);
 				exit(0);
 		}
+		if (!(c2 = fork()))
+		{
+				consumer(NULL);
+				exit(0);
+		}
 		
-		sem_unlink("sem_empty");
-		sem_unlink("sem_full");
-		sem_unlink("sem_mutex");
+		sem_unlink("/sem_empty");
+		sem_unlink("/sem_full");
+		sem_unlink("/sem_mutex");
 
 		wait(NULL);
 		wait(NULL);
